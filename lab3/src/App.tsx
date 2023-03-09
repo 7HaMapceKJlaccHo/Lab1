0a6fc8d7267c73e6fa60ed4e531f27202ae919f7
Roma <terobank7near@gmail.com>
2023-03-10 00:29:13 +0200

    Incarcarea laborator 3

--- lab3/src/App.tsx
+++ lab3/src/App.tsx
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ResponsiveAppBar from "./components/appbar";
+import { Button, Card, TextField } from "@mui/material";
+import ActionAreaCard from "./components/card";
+
+const temparray = [1, 2, 3];
 
 function App() {
+  const [nume, setNume] = useState<string>();
+  const [prenume, setPrenume] = useState<string>();
+  const [grupa, setGrupa] = useState<string>();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <ResponsiveAppBar />
       </header>
+      <main className="contents">
+        <div>
+          <h1>Content</h1>
+        </div>
+        <div className="cards">
+          {temparray.map((id) => (
+            <div className="card" key={id}>
+              <ActionAreaCard />
+            </div>
+          ))}
+        </div>
+        <div>
+          <h1>Formular</h1>
+        </div>
+        <div>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              console.log(
+                `Nume= ${nume} \nPrenume= ${prenume} \nGrupa= ${grupa}`
+              );
+              alert(`Nume= ${nume} \nPrenume= ${prenume} \nGrupa= ${grupa}`);
+            }}
+          >
+            <TextField
+              required
+              id="nume"
+              label="Nume"
+              value={nume}
+              onChange={(event) => {
+                setNume(event.target.value);
+              }}
+            />
+            <TextField
+              required
+              id="prenume"
+              label="Prenume"
+              value={prenume}
+              onChange={(event) => {
+                setPrenume(event.target.value);
+              }}
+            />
+            <TextField
+              required
+              id="grupa"
+              label="Grupa"
+              value={grupa}
+              onChange={(event) => {
+                setGrupa(event.target.value);
+              }}
+            />
+            <Button type="submit" variant="contained">
+              Transmite
+            </Button>
+          </form>
+          <div>
+            <span>Nume:{nume} </span>
+          </div>
+          <div>
+            <span>Prenume:{prenume}</span>
+          </div>
+          <div>
+            <span>Grupa:{grupa}</span>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ lab3/src/components/card.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+export default function ActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://c8.alamy.com/comp/RDDK0C/adult-film-actress-angela-white-attends-the-xbiz-awards-at-hotel-westin-bonaventure-in-los-angeles-usa-on-17-january-2019-usage-worldwide-RDDK0C.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
